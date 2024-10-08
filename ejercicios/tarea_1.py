fb409443a9b2ae61222c2d6b95aea2df1eceac58
Eloy <esau.olivacs@gmail.com>
2024-10-08 11:13:59 -0600

    Create tarea_1.py

---
+++ ejercicios/tarea_1.py
@@ -0,0 +1,27 @@
+import unittest
+# Dados 2 strings devolver la suma de ambos
+# s1 = "10"
+# s2 = "20" 
+# resultado
+
+#hint: utilizar casting
+
+def add(string_1: str, string_2: str) -> int:
+  # Colocar aquí tu codigo
+  pass # Eliminar esta linea y colocar return
+
+
+assert add("10", "20") == 30
+assert add("234", "123") == 357
+assert add("-1", "1") == 0
+
+
+# Dado un string devuelver el string al reves
+
+
+def reversa(string_1: str) -> str:
+  # Colocar aquí tu codigo
+  pass # Eliminar esta linea y colocar return
+
+assert reversa("Hola") == "aloH"
+assert reversa("12345") == "54321"
