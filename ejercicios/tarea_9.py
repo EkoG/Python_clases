6d488c845b8634e32bf8f71bb5604ca7b9b61f6c
Eloy <esau.olivacs@gmail.com>
2024-10-30 07:20:44 +0000

    ejercicios clase 14.1

---
+++ ejercicios/tarea_9.py
@@ -0,0 +1,8 @@
+def count_word_occurrences(word_list):
+    # Tu código aquí
+    pass
+
+# Ejemplo de uso
+input_list = ["apple", "banana", "apple", "orange", "banana", "apple"]
+output_dict = count_word_occurrences(input_list)
+print(output_dict)  # Debería imprimir {"apple": 3, "banana": 2, "orange": 1}
