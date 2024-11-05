20a9115a91610dc1ea1237378d01a2be4092ebd2
Eloy <esau.olivacs@gmail.com>
2024-11-05 05:17:01 +0000

    clase 17

--- ejercicios/tarea_9.py
+++ ejercicios/tarea_9.py
@@ -1,8 +1,30 @@
-def count_word_occurrences(word_list):
-    # Tu código aquí
-    pass
+def count_word_occurrences(word_list: list) -> dict:
+    result = dict()
+    for fruit in word_list:
+        if fruit in result:
+            #
+            result[fruit] += 1
+        else:
+            result[fruit] = 1
+    return result
+
 
 # Ejemplo de uso
-input_list = ["apple", "banana", "apple", "orange", "banana", "apple"]
+input_list = ["apple", "apple", "orange", "banana", "apple"]
 output_dict = count_word_occurrences(input_list)
 print(output_dict)  # Debería imprimir {"apple": 3, "banana": 2, "orange": 1}
+
+# apple -> {'apple': 1}
+# apple -> {'apple' : 2 } 1 + 1 
+# orange -> {'orange': 1}
+# apple - {'apple': 3 } 2 + 1
+# acceder a la llave cosa
+print(output_dict.get('cosa')) # util 
+# print(output_dict['cosa'])
+print(output_dict.get('apple'))
+# print(output_dict.get('apple') + 1)
+output_dict['apple'] = output_dict.get('apple') + 1
+print(output_dict.get('apple'))
+
+# utiliza el metodo get para reescribir la función de arriba
+
